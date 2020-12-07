f19742f650a5935b18f5264acca9d738e076fc45
masQOD <qodirmasruri@gmail.com>
2020-12-07 17:42:49 +0700

    manual deploy

--- aurelia-bootstrap.5d892ba39680dc3649b9.bundle.js
+++ aurelia-bootstrap.5d892ba39680dc3649b9.bundle.js
@@ -79,7 +79,7 @@
 /******/ 		script.async = true;
 /******/ 		script.timeout = 120000;
 /******/
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"2aaf62a68413d4178d53","1":"2d9db9088c51f5c021fe","2":"d3ed207cf5496736d6ee"}[chunkId] + ".chunk.js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"02994d3dc4e4645d9a86","1":"2d9db9088c51f5c021fe","2":"d3ed207cf5496736d6ee"}[chunkId] + ".chunk.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
